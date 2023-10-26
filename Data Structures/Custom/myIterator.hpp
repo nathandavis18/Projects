b639093676b5accdb7a379a09fa1cc5f09dcd45b
nathandavis18 <nathandavis8@mail.weber.edu>
2023-10-25 21:53:59 -0600

    Sort is now merge sort

--- Data Structures/Custom/myIterator.hpp
+++ Data Structures/Custom/myIterator.hpp
@@ -20,15 +20,11 @@ namespace custom{
         myIterator& operator--() { ptr--; return *this; } //Increment operators 
         myIterator operator--(int) { myIterator tmp = *this; --(*this); return tmp; }
         myIterator& operator+=(const difference_type m) { ptr += m; return *this; }
-        myIterator& operator-=(const difference_type m) { ptr -= m; return *this; }
-        
+        myIterator& operator-=(const difference_type m) { ptr -= m; return *this; }        
 
-
-        friend myIterator operator-(const myIterator& it, const difference_type m) { myIterator tmp = it; tmp -= m; return tmp;  }
-        friend myIterator operator+(const myIterator& it, const difference_type m) { myIterator tmp = it; tmp += m; return tmp; }
-        friend myIterator operator-(const difference_type m, const myIterator& it) { return it - m; }
-        friend myIterator operator+(const difference_type m, const myIterator& it) { return it + m; }
-        friend difference_type operator-(const myIterator& a, const myIterator& b) { return (a.ptr - b.ptr); }
+        constexpr myIterator operator-(const difference_type m) const noexcept { return myIterator(ptr - m); }
+        constexpr myIterator operator+(const difference_type m) const noexcept { return myIterator(ptr + m); }
+        friend auto operator-(const myIterator& lhs, const myIterator& rhs) { return (lhs.ptr - rhs.ptr); }
         friend auto operator<=>( const myIterator&, const myIterator& ) = default;
     private:
         T* ptr;
--- Data Structures/Custom/sort.hpp
+++ Data Structures/Custom/sort.hpp
@@ -1,123 +1,72 @@
 #ifndef SORT
 #define SORT
-#include <cmath>
 namespace detail{
     template<class inputIter>
-    inputIter iLeftChild(inputIter i){
-        return i + (2 * (i - inputIter(0)) + 1);
-    }
+    void merge(inputIter& start, inputIter& mid, inputIter& last){      
+        const size_t subLeft = (mid - start) + 1;
+        const size_t subRight = (last - mid);
 
-    template<class inputIter>
-    inputIter iParent(inputIter i){
-        return i + (i - (i + 1)) / 2;
-    }
+        using T = typename std::iterator_traits<inputIter>::value_type;
+        T* left = new T[subLeft];
+        T* right = new T[subRight];
 
-    template<class inputIter>
-    void siftDown(inputIter &begin, inputIter& end){
-        while(iLeftChild(begin) < end){
-            inputIter child = iLeftChild(begin);
-            if(child + 1 < end && *child < *(child + 1)){
-                ++child;
-            }
-            if(*begin < *child){
-                std::swap(*begin, *child);
-                begin = child;
+        for(int i = 0; i < subLeft; ++i){
+            left[i] = *(start + i);
+        }
+        for(int i = 0; i < subRight; ++i){
+            right[i] = *(mid + 1 + i);
+        }
+
+        size_t indexLeft = 0, indexRight = 0, indexMerge = 0;
+        while(indexLeft < subLeft && indexRight < subRight){
+            if(left[indexLeft] <= right[indexRight]){
+                *(start + indexMerge) = left[indexLeft];
+                ++indexLeft;
             }
             else{
-                return;
+                *(start + indexMerge) = right[indexRight];
+                ++indexRight;
             }
+            ++indexMerge;
         }
-    }
 
-    template<class inputIter>
-    void heapify(inputIter& begin, inputIter& end){
-        inputIter start = begin;
-        start = iParent(end + 1);
-        while(start > 0){
-            --start;
-            siftDown(start, end);
-        }
-    }
+        while(indexLeft < subLeft){
+            *(start + indexMerge) = left[indexLeft];
+            ++indexLeft;
+            ++indexMerge;
+        } 
 
-    template<class inputIter>
-    inputIter partition(inputIter& begin, inputIter& end){
-        inputIter mid = begin + (end - begin) / 2;
-        inputIter start = begin;
-        inputIter last = end;
-        while(true){
-            while(*start < *mid){
-                ++start;
-            }
-            --last;
-            while(*last > *mid){
-                --last;
-            }
-            if(!(start < last)){
-                return last;
-            }
-            std::swap(*start, *last);
-            ++start;
+        while(indexRight < subRight){
+            *(start + indexMerge) = right[indexRight];
+            ++indexRight;
+            ++indexMerge;
         }
-    }
-    
-    template<class inputIter>
-    void insertionSort(inputIter& begin, inputIter& end){
-        inputIter start = begin;
-        ++start;
-        while(start < end){
-            inputIter it = start;
-            while(it > begin){
-                if(*(it - 1) > *it){
-                    std::swap(*it, *(it - 1));
-                }
-                --it;
-            }
-            ++start;
-        }
-    }
 
-    template<class inputIter>
-    void heapSort(inputIter& begin, inputIter& end){
-        size_t length = end - begin;
-        heapify(begin, end);
-
-        while(end > (begin + 1)){
-            --end;
-            std::swap(*end, *begin);
-            siftDown(begin, end);
-        }
+        delete[] left;
+        delete[] right;
     }
 }
 
 namespace custom{
     template<class inputIter>
-    void introsortLoop(inputIter&begin, inputIter& end, size_t max_depth){
-        size_t length = end - begin;
-        if(length < 16){
-            detail::insertionSort(begin, end);
-        }
-        else if(max_depth == 0){
-            detail::heapSort(begin, end);
-        }
-        else{
-            inputIter p = detail::partition(begin, end);
-            introsortLoop(begin, p, max_depth - 1);
-            inputIter p2 = p + 1;
-            introsortLoop(p2, end, max_depth - 1);
-        }
-    }
-    template<class inputIter>
-    void _sort(inputIter& begin, inputIter& end, size_t max_depth){
-        if(begin != end){
-            introsortLoop(begin, end, max_depth);
-            detail::insertionSort(begin, end);
-        }
+    void _sort(inputIter& start, inputIter& last){
+        if(start >= last)
+            return;
+        
+        inputIter mid = start + ((last - 1) - start) / 2;
+        inputIter mid2 = mid + 1;
+
+        _sort(start, mid);
+        _sort(mid2, last);
+        detail::merge(start, mid, last);
     }
 
     template<class inputIter>
-    void sort(inputIter begin, inputIter end){
-        size_t max_depth = log2(end - begin);
-        _sort(begin, end, max_depth);
+    void sort(inputIter& start, inputIter& last){
+        if(start >= last) return;
+
+        inputIter end = last - 1;
+        _sort(start, end);
     }
 }
 #endif //SORT
